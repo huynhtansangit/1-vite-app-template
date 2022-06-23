8aabe84dc90992677b8ea2f66b1277c972e9ed15
Huynh Tan Sang (SX/BSV35-EA) <hsy1hc@bosch.com>
2022-06-23 17:32:53 +0700

    Add reusable Button and manage the user input state

--- src/Components/Users/AddUser.jsx
+++ src/Components/Users/AddUser.jsx
@@ -1,19 +1,30 @@
-import React from "react";
-import Card from "../Card/Card";
+import React,{useState} from "react";
+import Card from "../UI/Card/Card";
 import styles from "./AddUser.module.scss";
+import Button from "../UI/Button/Button";
 const AddUser = () => {
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername,enteredAge)
   };
-
+  const usernameChangeHandler=(event)=>{
+    setEnteredUsername(event.target.value);
+  }
+  const ageChangeHandler=(event)=>{
+    setEnteredAge(event.target.value);
+  }
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" />
+        <input type="text" name="username" id="username" onChange={usernameChangeHandler}/>
         <label htmlFor="age">Age</label>
-        <input type="number" name="age" id="age" />
-        <button type="submit">Submit</button>
+        <input type="number" name="age" id="age" onChange={ageChangeHandler}/>
+        <Button type="submit">Submit</Button>
       </form>
     </Card>
   );
